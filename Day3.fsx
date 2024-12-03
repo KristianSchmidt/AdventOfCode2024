f245e28dc0f47d149925a1d78680a93649bbdf82
Kristian Schmidt <kristian@kreutz.us>
2024-12-03 07:05:06 +0100

    Day 3 part 1

--- Day3.fsx
+++ Day3.fsx
@@ -1,12 +1,25 @@
 #load "Helpers.fsx"
 
 open System
+open System.Text.RegularExpressions
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
+
 let data = Helpers.Web.getInput 3
 
-let ans1 = data
+let getLineSum s =
+    let ms = System.Text.RegularExpressions.Regex.Matches(s, "mul\((\d+),(\d+)\)")
+
+    ms
+    |> Seq.sumBy
+        (fun m -> 
+            printfn "%s" m.Groups[0].Value;
+            (int64 m.Groups[1].Value)*(int64 m.Groups[2].Value))
+
+let ans1 = data |> Seq.sumBy getLineSum
+
+data[0]
 
 ans1
 
