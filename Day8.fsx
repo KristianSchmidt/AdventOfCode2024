48b0d859254aa59fdf441a50a20d47a2ec3f62ac
Kristian Schmidt <kristian@kreutz.us>
2024-12-08 14:01:34 +0100

    Day 8 solution

--- Day8.fsx
+++ Day8.fsx
@@ -4,14 +4,55 @@ open System
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 8
+let data =
+    Helpers.Web.getInput 8
+    |> Array.map (fun s -> s.ToCharArray())
 
-let ans1 = data
+let coords =
+    seq {
+        for x in 0 .. data.Length - 1 do
+            for y in 0 .. data.Length - 1 do
+                yield (x,y)
+    }
+
+let isInside (x,y) = 0 <= x && x <= data.Length - 1 && 0 <= y && y <= data.Length - 1
+
+let ans1 =
+    seq {
+        for (x,y) in coords do
+            for (x', y') in coords do
+                if ((x,y) <> (x',y') &&
+                    data[x][y] = data[x'][y']
+                    && data[x][y] <> '.') then
+                    yield x'+(x'-x), y'+(y'-y)
+    }
+    |> Seq.filter isInside
+    |> Set.ofSeq
+    |> Set.count
 
 ans1
 
 /// Part 2
 
-let ans2 = data
+let ans2 =
+    seq {
+        for (x,y) in coords do
+            for (x', y') in coords do
+                if ((x,y) <> (x',y') &&
+                    data[x][y] = data[x'][y']
+                    && data[x][y] <> '.') then
+                    yield (x,y)
+                    yield (x',y')
+                    let (vx,vy) = (x'-x), (y'-y)
+                    let mutable i = 1
+                    let mutable candidate = ((x'+i*vx),(y'+i*vy))
+                    while (isInside candidate) do
+                        yield candidate
+                        i <- i + 1
+                        candidate <- ((x'+i*vx),(y'+i*vy))
+    }
+    |> Seq.filter isInside
+    |> Set.ofSeq
+    |> Set.count
 
 ans2
