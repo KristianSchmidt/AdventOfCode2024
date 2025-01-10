7f0c8cc6d32f5b96715cba2a0c7050f0ee59af34
Kristian Schmidt <kristian@kreutz.us>
2025-01-10 12:22:38 +0100

    Day 25 solution

--- Day25.fsx
+++ Day25.fsx
@@ -4,9 +4,34 @@ open System
 
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-let data = Helpers.Web.getInput 25
+let data =
+    let i = Helpers.Web.getInput 25
+    i
+    |> Array.chunkBySize 8
+    |> Array.map (fun arr -> arr[0..6])
 
-let ans1 = data
+let toNumSeq (arr : string array) =
+    [|0..4|]
+    |> Array.map (fun i -> 
+        arr[1..]
+        |> Array.map (fun s -> s[i])
+        |> Array.filter ((=)'#')
+        |> Array.length)
+
+let locks,keys =
+    let (l,k) =
+        data
+        |> Array.partition (fun arr -> arr[0][0] = '#')
+    l |> Array.map toNumSeq, k |> Array.map (Array.rev >> toNumSeq)
+
+let fits (a1: int array,a2: int array) =
+    Array.zip a1 a2
+    |> Array.forall (fun (x1,x2) -> x1 + x2 <= 5)
+
+let ans1 =
+    Array.allPairs locks keys
+    |> Array.filter fits
+    |> Array.length
 
 ans1
 
